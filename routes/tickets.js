dd844b605b9e2321b88d0ac59932c206d53ad3fe
jamesonhussey <jamesonhussey99@gmail.com>
2024-03-20 15:19:15 -0500

    Styling

--- routes/tickets.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const ticketsCtrl = require('../controllers/tickets')
-
-router.get('/tickets/:id/new', ticketsCtrl.new)
-
-router.post('/tickets/:id', ticketsCtrl.create)
-
-module.exports = router
